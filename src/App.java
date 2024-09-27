4e2e47118eb6b98bf9c7a5da426502971817c246
CaioBalta <caio.crbaltazar@gmail.com>
2024-09-27 09:48:08 -0300

    Exercicios - Lista Java

---
+++ src/App.java
@@ -0,0 +1,22 @@
+public class App {
+    public static void main(String[] args) throws Exception 
+    {
+        Aluno aluno1 = new Aluno();
+        aluno1. setNome ("Caio");
+        Aluno aluno2 = new Aluno();
+        aluno2. setNome ("Balta");
+        Aluno aluno3 = new Aluno();
+        aluno3. setNome ("Vitooor");
+        
+        System.out.println(aluno2.getNome());
+        System.out.println(aluno1.equals(aluno2));
+        System.out.println(aluno2.equals(aluno3));
+        
+        Vetor vetor = new Vetor();
+        vetor.add(aluno1);
+        vetor.add(3, aluno2);
+        System.out.println(vetor.toString());
+
+        vetor.add(111, aluno3);
+    }
+}
